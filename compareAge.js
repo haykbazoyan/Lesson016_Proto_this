54aff5d4f8ed3c2b93cddb3ee2a09ace3e563a69
Hayk Bazoyan <bazoyan13@gmail.com>
2021-05-21 13:45:34 +0400

    feat: compare age using func constructor

---
+++ compareAge.js
@@ -0,0 +1,24 @@
+// Compare Ages
+
+function Person(name, age) {
+  this.name = name;
+  this.age = age;
+
+  this.compareAge = (secPerson) => {
+    if (this.age > secPerson.age) {
+      return `I am older than ${secPerson.name}.`;
+    } else if (this.age === secPerson.age) {
+      return `${secPerson.name} is the same age as me.`;
+    } else {
+      return `${secPerson.name} is older than me.`;
+    }
+  };
+}
+
+const p1 = new Person("Samuel", 24);
+const p2 = new Person("Joel", 36);
+const p3 = new Person("Lily", 24);
+
+console.log(p1.compareAge(p2)); // ➞ "Joel is older than me."
+console.log(p2.compareAge(p1)); // ➞ "Samuel is younger than me."
+console.log(p1.compareAge(p3)); // ➞ "Lily is the same age as me."
